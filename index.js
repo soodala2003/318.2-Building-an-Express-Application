6d9d32c8c0624e7b932ca13717df5e061c44424e
soodala2003 <soodala78@gmail.com>
2024-11-02 16:55:00 -0400

    updated index.js file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const path = require("path");
 
 const app = express();
@@ -6,9 +8,11 @@ const port = 3000;
 
 const userRoutes = require("./routes/users");
 const postRoutes = require("./routes/posts");
+const loginRoutes = require("./routes/login");
 
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+app.use("/login", loginRoutes);
 
 
 app.set("view engine", "pug"); // register the template engine
@@ -16,12 +20,8 @@ app.set("view engine", "pug"); // register the template engine
 app.set("views", path.join(__dirname, "views"));
 
 // Parsing Middleware
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json({ extended: true }));
-
-// Use our Routes
-//app.use("/api/users", users);
-//app.use("/api/posts", posts);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Keeping it simple.");
